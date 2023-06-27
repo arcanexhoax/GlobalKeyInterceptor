855f067cfd3beffb174591bc6e5d852d6abe64b3
hoax <ninjaa176@gmail.com>
2023-06-27 23:11:46 +0300

    add xml comments

--- GlobalKeyInterceptor/KeyHooker.cs
+++ GlobalKeyInterceptor/KeyHooker.cs
@@ -10,8 +10,16 @@ namespace GlobalKeyInterceptor
         private readonly KeyHookerNative _hooker;
         private readonly ConsoleKey[] _hookingKeys;
 
+        /// <summary>
+        /// An event that invokes when any of the specified keys was pressed.
+        /// </summary>
         public event EventHandler<KeyHookedEventArgs> KeyHooked;
 
+        /// <summary>
+        /// A class that intercept specified keys. To receive intercepted keys, use <see cref="KeyHooked"/> event.
+        /// </summary>
+        /// <param name="hookingKeys">A list of keys that will be intercepted. If parameter is empty, every key will be intercepted.</param>
+        /// <remarks> Disposable</remarks>
         public KeyHooker(params ConsoleKey[] hookingKeys)
         {
             _hooker = new KeyHookerNative();
@@ -25,30 +33,42 @@ namespace GlobalKeyInterceptor
                 return;
 
             ConsoleKey key = (ConsoleKey)e.KeyData.VirtualCode;
-            KeyModifier ctrlModifier = KeyModifier.None;
-            KeyModifier shiftModifier = KeyModifier.None;
-            KeyModifier altModifier = KeyModifier.None;
+            
+            if (_hookingKeys.Length == 0)
+            {
+                InvokeKeyHooked(key);
+                return;
+            }
 
             foreach (var hook in _hookingKeys)
             {
                 if (hook == key)
                 {
-                    bool ctrlPressed = NativeMethods.GetAsyncKeyState(KeyHookerNative.VkLeftCtrl) > 1 ||
-                        NativeMethods.GetAsyncKeyState(KeyHookerNative.VkRightCtrl) > 1;
-                    bool shiftPressed = NativeMethods.GetAsyncKeyState(KeyHookerNative.VkLeftShift) > 1 ||
-                        NativeMethods.GetAsyncKeyState(KeyHookerNative.VkRightShift) > 1;
-                    bool altPressed = NativeMethods.GetAsyncKeyState(KeyHookerNative.VkLeftAlt) > 1 ||
-                        NativeMethods.GetAsyncKeyState(KeyHookerNative.VkRightAlt) > 1;
-
-                    if (ctrlPressed) ctrlModifier = KeyModifier.Ctrl;
-                    if (shiftPressed) shiftModifier = KeyModifier.Shift;
-                    if (altPressed) altModifier = KeyModifier.Alt;
-
-                    KeyHooked?.Invoke(this, new KeyHookedEventArgs(key, ctrlModifier | shiftModifier | altModifier));
-                    //e.Handled = true;
+                    InvokeKeyHooked(hook);
                     break;
                 }
             }
+        }
+
+        private void InvokeKeyHooked(ConsoleKey key)
+        {
+            KeyModifier ctrlModifier = KeyModifier.None;
+            KeyModifier shiftModifier = KeyModifier.None;
+            KeyModifier altModifier = KeyModifier.None;
+
+            bool ctrlPressed = NativeMethods.GetAsyncKeyState(KeyHookerNative.VkLeftCtrl) > 1 ||
+                        NativeMethods.GetAsyncKeyState(KeyHookerNative.VkRightCtrl) > 1;
+            bool shiftPressed = NativeMethods.GetAsyncKeyState(KeyHookerNative.VkLeftShift) > 1 ||
+                NativeMethods.GetAsyncKeyState(KeyHookerNative.VkRightShift) > 1;
+            bool altPressed = NativeMethods.GetAsyncKeyState(KeyHookerNative.VkLeftAlt) > 1 ||
+                NativeMethods.GetAsyncKeyState(KeyHookerNative.VkRightAlt) > 1;
+
+            if (ctrlPressed) ctrlModifier = KeyModifier.Ctrl;
+            if (shiftPressed) shiftModifier = KeyModifier.Shift;
+            if (altPressed) altModifier = KeyModifier.Alt;
+
+            KeyHooked?.Invoke(this, new KeyHookedEventArgs(key, ctrlModifier | shiftModifier | altModifier));
+            //e.Handled = true;
         }
 
         public void Dispose()
--- GlobalKeyInterceptor/Model/KeyHookedEventArgs.cs
+++ GlobalKeyInterceptor/Model/KeyHookedEventArgs.cs
@@ -5,13 +5,22 @@ namespace GlobalKeyInterceptor.Model
 {
     public class KeyHookedEventArgs : EventArgs
     {
+        /// <summary>Intercepted key.</summary>
+        /// <remarks>To get VK code of the key use <see cref="int"/> casting.</remarks>
         public ConsoleKey Key { get; }
+
+        /// <summary>A modifier of the intercepted shortcut.</summary>
+        /// <remarks>
+        /// If modifier isn't present, the value <see cref="KeyModifier.None"/> is set.
+        /// <br/>The value can contain multiple modifiers, to check if modifier is intercepted, use <see cref="System.Enum.HasFlag(System.Enum)"/>.
+        /// <code>bool isCtrlIntercepted = e.Modifier.HasFlag(KeyModifier.Ctrl);</code>
+        /// </remarks>
         public KeyModifier Modifier { get; }
 
         public KeyHookedEventArgs(ConsoleKey key, KeyModifier modifier)
         {
             Key = key;
-            Modifier = modifier;
+            Modifier = modifier; 
         }
     }
 }
